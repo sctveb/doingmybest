94614d0d95c4edc71301eaefabfe6672747cca82
sctveb <sctveb@gmail.com>
2020-05-03 00:09:45 +0900

    2020-05-03 redux-actions

--- src/actions/ActionTypes.js
+++
@@ -1,7 +0,0 @@
-// Action Types
-export const CREATE = 'CREATE';
-export const REMOVE = 'REMOVE';
-
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const SET_COLOR = 'SET_COLOR';
---
+++ src/modules/index.js
@@ -0,0 +1,75 @@
+import { createAction, handleActions } from 'redux-actions';
+import { Map, List } from 'immutable';
+
+// Action Types
+const CREATE = 'counter/CREATE';
+const REMOVE = 'counter/REMOVE';
+const INCREMENT = 'counter/INCREMENT';
+const DECREMENT = 'counter/DECREMENT';
+const SET_COLOR = 'counter/SET_COLOR';
+
+// Action Creators
+export const create = createAction(CREATE); // color
+export const remove = createAction(REMOVE);
+export const increment = createAction(INCREMENT); // index
+export const decrement = createAction(DECREMENT); // index
+export const setColor = createAction(SET_COLOR); // { index, color }
+
+// init state
+const initialState = Map({
+  counters: List([
+    Map({
+      color: 'black',
+      number: 0,
+    }),
+  ]),
+});
+
+export default handleActions(
+  {
+    [CREATE]: (state, action) => {
+      const counters = state.get('counters');
+      return state.set(
+        'counters',
+        counters.push(
+          Map({
+            color: action.payload,
+            number: 0,
+          }),
+        ),
+      );
+    },
+    [REMOVE]: (state, action) => {
+      const counters = state.get('counters');
+      return state.set('counters', counters.pop());
+    },
+    [INCREMENT]: (state, action) => {
+      const counters = state.get('counters');
+      return state.set(
+        'counters',
+        counters.update(action.payload, (counter) =>
+          counter.set('number', counter.get('number') + 1),
+        ),
+      );
+    },
+    [DECREMENT]: (state, action) => {
+      const counters = state.get('counters');
+      return state.set(
+        'counters',
+        counters.update(action.payload, (counter) =>
+          counter.set('number', counter.get('number') - 1),
+        ),
+      );
+    },
+    [SET_COLOR]: (state, action) => {
+      const counters = state.get('counters');
+      return state.set(
+        'counters',
+        counters.update(action.payload.index, (counter) =>
+          counter.set('color', action.payload.color),
+        ),
+      );
+    },
+  },
+  initialState,
+);
